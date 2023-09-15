233e48233ce8cd377e95e2a04728597ae1ddfc0d
apoio-forLEARN <apoio@forlearn.ao>
2023-09-15 10:21:15 +0100

    replace ai model

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import {fastify} from 'fastify';
+import { fastifyCors } from '@fastify/cors';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-video';
 import { createTranscriptionRoute } from './routes/create-trascription';
+import { generateTransitionRoute } from './routes/generator-trascription';
 
 const app = fastify();
+
+app.register(fastifyCors, {
+    origin: '*'
+})
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
+app.register(generateTransitionRoute);
 
 app.listen({
     port: 3333
